e3dfd1ec8f6245f63dcaf1da2f22ce2f0246b3bc
kishorchouhan <devkishorbwr@gmail.com>
2019-06-21 14:25:23 +0530

    Got current geolocation i.e. coordinates.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import { component } from 'react';
+import React, { Component } from 'react';
+import './App.css';
+// import Weather from './Weather';
 
-export default class App extends component {
+
+// const API_KEY = '104ec94c9efb96e5545065b88c1f59c1';
+
+export default class App extends Component {
+	state = {
+		errorMessage: '',
+	}
+
+	componentDidMount() {
+		if (navigator.geolocation) {
+
+			navigator.geolocation.getCurrentPosition(((position) => {
+				const latitude = position.coords.latitude;
+				const longitude = position.coords.longitude;
+				console.log(`${latitude}, ${longitude}`);
+			}), () => {
+				this.setState({ errorMessage: 'Allow browser to access your location', });
+			})
+		} else {
+			this.setState({ errorMessage: 'Geolocation is not supported by your browser', });
+		}
+	};
 
   render() {
-    return;
-  }
+		const { errorMessage } = this.state;
+
+    return (
+			<div>
+				<p>{errorMessage}</p>
+			</div>
+		);
+  };
 };
