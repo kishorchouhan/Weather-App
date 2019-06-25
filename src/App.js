9aa741500c179a44099c6d4ddae4fde4a7246968
kishorchouhan <devkishorbwr@gmail.com>
2019-06-25 19:06:17 +0530

    Completed current location weather app. Styling and 7 day history left.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
-import Weather from './Weather';
+import React, { Component } from "react";
+import "./App.css";
+import Weather from "./Weather";
 
-
-const API_KEY = '104ec94c9efb96e5545065b88c1f59c1';
+const API_KEY = "104ec94c9efb96e5545065b88c1f59c1";
 
 export default class App extends Component {
-	state = {
-		errorMessage: '',
-		weatherData: {},
-	}
+  state = {
+    errorMessage: "",
+    weatherData: {},
+    isLoading: true,
+  };
 
-	componentDidMount() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(((position) => {
-				const latitude = position.coords.latitude;
-				const longitude = position.coords.longitude;
-				console.log(`${latitude}, ${longitude}`);
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          console.log(`${latitude}, ${longitude}`);
 
-				// Get Weather Data
-				(async () => {
-					const getWeatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
-					const weatherData = await getWeatherData.json();
-					console.log(weatherData);
-					this.setState({ weatherData });
-				})().catch(err => {
-					console.error('error', err);
-				});
-
-			}), () => {
-				this.setState({ errorMessage: 'Allow browser to access your location', });
-			})
-		} else {
-			this.setState({ errorMessage: 'Geolocation is not supported by your browser', });
-		}
-	};
+          // Get Weather Data
+          (async () => {
+            const getWeatherData = await fetch(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+            );
+            const weatherData = await getWeatherData.json();
+            console.log(weatherData);
+            this.setState({ weatherData, isLoading: false });
+          })().catch(err => {
+            console.error("error", err);
+          });
+        },
+        () => {
+          this.setState({
+            errorMessage: "Allow browser to access your location"
+          });
+        }
+      );
+    } else {
+      this.setState({
+        errorMessage: "Geolocation is not supported by your browser"
+      });
+    }
+  }
 
   render() {
-		const { errorMessage, weatherData } = this.state;
-
+    const { errorMessage, weatherData } = this.state;
     return (
-			<div>
-				<p>{errorMessage}</p>
-				<Weather weatherData = {weatherData}/>
-			</div>
-		);
-  };
-};
+      this.state.isLoading ? (<p>Loading</p>) : (
+        <div>
+          <p>{errorMessage}</p>
+          <Weather weatherData={weatherData} />
+        </div>
+      )
+    );
+  }
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React from "react";
 
-export default function Weather (props) {
-    const { weatherData } = props;
-    return (
-        <p>{weatherData.name}</p>
-    );
+export default function Weather(props) {
+  const { weatherData } = props;
+  let date = new Date(weatherData.dt * 1000);
+  return (
+    <div>
+      <div className="currentWeather">
+        <p>Your current city: {weatherData.name}, {weatherData.sys.country}</p>
+        <p>Current date and time: {date.toLocaleString()}</p>
+        <p>Weather: {weatherData.weather[0].description}</p>
+        <p>Temperature: {weatherData.main.temp} °C</p>
+        <p>Pressure: {weatherData.main.pressure} hPa</p>
+        <p>Humidity: {weatherData.main.humidity} %</p>
+        <p>Wind speed: {weatherData.wind.speed} meter/sec</p>
+      </div>
+      <div className="sevenDayHistory"></div>
+    </div>
+  );
 }
+/* 
+Sample Api Response for current location:
+  {"coord": { "lon": 139,"lat": 35},
+    "weather": [
+      {
+        "id": 800,
+        "main": "Clear",
+        "description": "clear sky",
+        "icon": "01n"
+      }
+    ],
+    "base": "stations",
+    "main": {
+      "temp": 289.92,
+      "pressure": 1009,
+      "humidity": 92,
+      "temp_min": 288.71,
+      "temp_max": 290.93
+    },
+    "wind": {
+      "speed": 0.47,
+      "deg": 107.538
+    },
+    "clouds": {
+      "all": 2
+    },
+    "dt": 1560350192,
+    "sys": {
+      "type": 3,
+      "id": 2019346,
+      "message": 0.0065,
+      "country": "JP",
+      "sunrise": 1560281377,
+      "sunset": 1560333478
+    },
+    "timezone": 32400,
+    "id": 1851632,
+    "name": "Shuzenji",
+    "cod": 200
+  }
+*/
